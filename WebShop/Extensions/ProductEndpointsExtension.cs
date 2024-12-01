f7e4591c5f1f4f854ed35c7a1e790409ca50afca
arybbe <adam.ryberg@live.se>
2024-12-01 23:48:21 +0100

    gjfj

---
+++ WebShop/Extensions/ProductEndpointsExtension.cs
@@ -0,0 +1,30 @@
+﻿using WebShop.Repositories;
+using WebShop.UnitOfWork;
+
+namespace WebShop.Extensions;
+
+public static class ProductEndpointsExtension 
+{
+    public static IEndpointRouteBuilder MapProductEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/products");
+
+        group.MapGet("/", GetAll);
+        group.MapPost("/", Add);
+
+        return app;
+    }
+
+    private static async Task<IResult> Add(IProductRepository repo, IUnitOfWork unitOfWork, Product product)
+    {
+        repo.Add(product);
+        unitOfWork.NotifyProductAdded(product);
+        return Results.Ok();
+    }
+
+    private static async Task<IResult> GetAll(IProductRepository repo)
+    {
+        var products = repo.GetAll();
+        return Results.Ok(products);
+    }
+}
--- WebShop/Program.cs
+++ WebShop/Program.cs
@@ -29,4 +29,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
 app.Run();
