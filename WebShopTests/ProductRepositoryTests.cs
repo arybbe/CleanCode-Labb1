f87b78bad7893c67effb8646f9f889aa6d99eec0
arybbe <adam.ryberg@live.se>
2025-05-12 19:33:58 +0200

    agagin

--- WebShopTests/ProductRepositoryTests.cs
+++ WebShopTests/ProductRepositoryTests.cs
@@ -3,19 +3,7 @@ using WebShop;
 
 namespace WebShopTests;
 
-[TestClass]
 public class ProductRepositoryTests
 {
-    [TestMethod]
-    public void Add_Then_GetAll_ReturnsAddedProduct()
-    {
-        var repo = new ProductRepository();
-        var p = new Product { Id = 1, Name = "X" };
-
-        repo.Add(p);
-        var all = repo.GetAll().ToList();
-
-        Assert.AreEqual(1, all.Count);
-        Assert.AreEqual("X", all[0].Name);
-    }
+    
 }
